3acc3f654d6b60fd3b113aa8acef35f9e812ed2a
Zicka67 <art.book@hotmail.fr>
2023-05-04 10:05:40 +0200

    Commencement de la créations des modules + ajout de length dans la vue de session et detailsession

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Modules;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ModuleController extends AbstractController
 {
@@ -15,4 +18,43 @@ class ModuleController extends AbstractController
             'controller_name' => 'ModuleController',
         ]);
     }
+
+    #[Route('/modules/new', name: 'modules_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        //Creation d'un module 
+        $module = new Modules();
+        //Creation d'un form avec les données de moduletype
+        $form = $this->createForm(ModulesType::class, $module);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //prepare
+            $entityManager->persist($module);
+            //execute
+            $entityManager->flush();
+
+            return $this->redirectToRoute('modules_show', ['id' => $module->getId()]);
+        }
+
+        return $this->render('modules/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/modules/{id}', name: 'modules_show', methods: ['GET'])]
+    public function show(Modules $module): Response
+    {
+        return $this->render('modules/show.html.twig', [
+            'module' => $module,
+        ]);
+    }
 }
+
+
+
+
+
+
+
