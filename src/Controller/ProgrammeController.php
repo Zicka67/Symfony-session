4b2e9e4f66758e05f39e91accbd83c31f73fcd22
Zicka67 <art.book@hotmail.fr>
2023-05-09 11:17:44 +0200

    Creation d'un from pour créer des formations ( modules normalement ) + création de la vue, et modification de detailSession pour afficher la liste de module de la session

--- src/Controller/ProgrammeController.php
+++ src/Controller/ProgrammeController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Programme;
+use App\Form\ProgrammeType;
+use App\Form\AddModuleFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -27,4 +30,26 @@ class ProgrammeController extends AbstractController
             'programmes' => $programmes,
         ]);
     }
+
+    #[Route('/programme/create', name: 'programme_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $programme = new Programme();
+        $form = $this->createForm(ProgrammeType::class, $programme);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($programme);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le programme a été créé avec succès !');
+
+            return $this->redirectToRoute('app_module');
+        }
+
+        return $this->render('programme/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
---
+++ src/Form/ProgrammeType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Programme;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ProgrammeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('session', EntityType::class, [
+                'class' => 'App\Entity\Session',
+                'choice_label' => 'title_section',
+            ])
+            ->add('modules', EntityType::class, [
+                'class' => 'App\Entity\Modules',
+                'choice_label' => 'title_modules',
+            ])
+            ->add('duree')
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Programme::class,
+        ]);
+    }
+}
